481ebfff6eafbcb0382bc51d5f189736d5bfecda
kelvinkip <kelvinkipla@gmail.com>
2022-04-29 21:55:53 +0300

    test to see if we can safe muyltiple contacts at once

--- credentials.py
+++ credentials.py
@@ -24,3 +24,20 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_contact.last_name, "kiplangat")
         self.assertEqual(self.new_contact.phone_number, "0728335615")
         self.assertEqual(self.new_contact.email, "kelvinkipla@gmail.com")
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        user.contact_list = [] # Empty the contact_list
+
+
+    def test_save_contact(self):
+        self.new_contact.save_contact()
+        self.assertEqual(len(user.contact_list), 1)
+
+    def test_save_multiple_contact(self):
+        self.new_contact.save_contact()
+        test_contact = user("Test", "User", "0728335615","test@user.com")
+        test_contact.save_contact()
+        self.assertEqual(len(user.contact_list), 2)
