f263cd0ed58d8608d3316583a47fed1d8b0c157c
kelvinkip <kelvinkipla@gmail.com>
2022-04-29 21:23:14 +0300

    added method that will return users that marches username

--- user.py
+++ user.py
@@ -30,4 +30,17 @@ class User:
         '''
         
         User.user_list.remove(self)
+    @classmethod
+    def find_by_user(cls, user_name):
+        '''
+        Method that takes in a user and returns a user that matches that username.
+        Args:
+            name: name to search for
+        Returns :
+            name of person that matches the password.
+        '''
+        
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                return user
         
