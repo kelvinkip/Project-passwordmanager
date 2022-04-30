6a9b8357b34233ba512130472f631de00ed82c53
kelvinkip <kelvinkipla@gmail.com>
2022-04-30 12:20:17 +0300

    function to delete user credentials

--- run.py
+++ run.py
@@ -28,3 +28,34 @@ def find_user(username):
     function that finds user by name and returns the user
     '''
     return User.find_by_user(username)
+def user_exists(username):
+    '''
+    function that checks if user exists and returns a boolean
+    '''
+    return User.user_exists(username)
+
+#credentials 
+def create_credentials(account,username,pass_word):
+    '''
+    function to create new credentials
+    '''
+    credentials = User(account,username,pass_word)
+    return credentials
+
+def save_credentials(credentials):
+    '''
+    function to save a credential
+    '''
+    credentials.save_credentials()
+    
+def delete_credential(credentials):
+    '''
+    function to delete credential
+    '''
+    credentials.delete_credentials()
+    
+def credential_exist(account):
+    '''
+    function that checks if  a credential exists by account and returns a boolean
+    '''
+    return User.find_credential_exist(account)
