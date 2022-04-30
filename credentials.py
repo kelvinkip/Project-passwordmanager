ed7dfcf6ad1c480645cddb6df232eab5566978e5
kelvinkip <kelvinkipla@gmail.com>
2022-04-30 15:58:51 +0300

    sort and error

--- credentials.py
+++ credentials.py
@@ -1,45 +1,27 @@
 class Credentials:
-    '''
-    class that generates new instances of user's credentials
-    '''
+   
     
     credentials_list = [] #empty credentials list
     
     def __init__(self,account,username,pass_word):
-        '''
-        init method to define properties of our objects
-        Args:
-                account = new account
-                username = new username
-                pass_word = new password
-        '''
+       
         
         self.account = account
         self.username = username
         self.pass_word = pass_word
         
     def save_credential(self):
-        '''
-        save_credential method that saves credentials objects into credentials list
-        '''
+       
         
         Credentials.credentials_list.append(self)
         
     def delete_credential(self):
-        '''
-        delete credential method deletes a saved credential in the credentials_list
-        '''
+        
         Credentials.credentials_list.remove(self)
         
     @classmethod
     def find_user_by_account(cls, account):
-        '''
-        Method that takes in a credential and returns a credential that matches that account.
-        Args:
-        account: account to search for
-        Returns :
-            credential of person that matches the account.
-        '''
+      
         
         for Credentials in cls.credentials_list:
             if Credentials.account == account:
@@ -47,13 +29,7 @@ class Credentials:
             
     @classmethod
     def find_credential_exist(cls,account):
-        '''
-        method that checks if a credential exists from the credentials list
-        Args:
-        account: account name to search if it exists
-        Returns:
-        Booleans: true or false if the credential exists
-        '''
+      
         
         for Credentials in cls.credentials_list:
             if Credentials.account == account:
@@ -63,9 +39,6 @@ class Credentials:
     
     @classmethod
     def display_credentials(cls):
-        '''
-        method that returns credentials list
-        '''
-        
+      
         return cls.credentials_list
     
--- run.py
+++ run.py
@@ -4,70 +4,50 @@ from credentials import Credentials
 import random
 
 def create_user(fullname,password):
-    '''
-    function  to create a new user
-    '''
+   
     
     new_user = User(fullname,password)
     return new_user
 
 def save_user(user):
-    '''
-    function to save user
-    '''
+   
     user.save_user()
     
 def delete_user(user):
-        '''
-        function to delete user
-        '''
+       
         user.delete_user()
         
 def find_user(username):
-    '''
-    function that finds user by name and returns the user
-    '''
+   
     return User.find_by_user(username)
 
 def user_exists(username):
-    '''
-    function that checks if user exists and returns a boolean
-    '''
+    
     return User.user_exists(username)
 
-#credentials 
-def create_credentials(account,username,pass_word):
-    '''
-    function to create new credentials
-    '''
-    Credentials = Credentials(account,username,pass_word)
-    return Credentials
-
-def save_credentials(Credentials):
-    '''
-    function to save a credential
-    '''
-    Credentials.save_credentials()
+def create_Credentials(account,username,pass_word):
     
-def delete_credentials(Credentials):
-    '''
-    function to delete credential
-    '''
-    Credentials.delete_credentials()
-    
-def user_exist(account):
-    '''
-    function that checks if  a credential exists by account and returns a boolean
-    '''
-    return Credentials.find_user_exist(account)
+    credentials = Credentials(account,username,pass_word)
+    return credentials
 
-def display_user():
-    '''
-    function that returns all saved credentials
-    '''
-    return Credentials.display_user()
+def save_credentials(credentials):
+    
+    credentials.save_credentials()
+    
+def delete_credential(credentials):
+
+    credentials.delete_credentials()
+    
+def credential_exist(account):
+    
+    return Credentials.find_credential_exist(account)
+
+def display_credentials():
+    
+    return Credentials.display_credentials()
+
 def main():
-    print("Welcome to Password Locker")
+    print("Welcome to Password manager")
     print("*" * 80)
     print("\n")
     
@@ -115,11 +95,11 @@ def main():
             
         elif short_code == 'DC':
             
-            if display_user():
+            if display_credentials():
                 print("Here are your credentials")
                 print("\n")
                 
-                for Credentials in display_user():
+                for Credentials in display_credentials():
                     print(f"{Credentials.account} \n {Credentials.password} \n {Credentials.username}")
                     print("\n")
             else:
